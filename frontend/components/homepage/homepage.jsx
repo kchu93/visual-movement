3ab7b6b3991a24dedf972701e5ed1ddf6ac8371c
kchu93 <kevinchu676@gmail.com>
2018-02-28 17:17:54 -0800

    Add styling to homepage

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -9,8 +9,17 @@ class Homepage extends React.Component {
         <div className="section1">
           <a href="/#/signup">
             <div className="section1-link">
+              <img className="left-side-image" src="https://i.imgur.com/vXydIFI.png"/>
             </div>
           </a>
+
+          <div className="section2">
+            <a href="/#/signup">
+              <div className="section2-link">
+                <img className="right-side-image" src="https://i.imgur.com/Kajf7x5.png"/>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     );
